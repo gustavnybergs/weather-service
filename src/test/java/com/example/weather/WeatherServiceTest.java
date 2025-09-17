3d6a920accf27593aa04f9a6961b0519b7faacf3
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-09-17 16:13:04 +0200

    Add: Test for WeatherAtSpecificLocation

--- src/test/java/com/example/weather/WeatherServiceTest.java
+++ src/test/java/com/example/weather/WeatherServiceTest.java
@@ -1,13 +1,10 @@
 package com.example.weather;
-
-import com.grupp3.weather.WeatherApiApplication;
 import com.grupp3.weather.service.WeatherService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.Collections;
@@ -16,7 +13,7 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class WeatherServiceTest {
 
@@ -51,6 +48,34 @@ public class WeatherServiceTest {
         assertNotNull(response);
         assertEquals("Stockholm", response.get("name"));
         assertEquals("Sweden", response.get("country"));
+    }
+
+
+    @Test
+    void testFetchCurrentWeatherAtSpecificLocation() {
+        String location = "Stockholm";
+
+        Map<String, Object> locationMap = new HashMap<>();
+        locationMap.put("name", "Stockholm");
+        locationMap.put("latitude", 59.3293);
+        locationMap.put("longitude", 18.0686);
+
+        List<Map<String, Object>> results = Collections.singletonList(locationMap);
+        Map<String, Object> apiResponse = new HashMap<>();
+        apiResponse.put("results", results);
+
+        when(restTemplate.getForObject("https://geocoding-api.open-meteo.com/v1/search?name=" + location, Map.class)).thenReturn(apiResponse);
+
+        WeatherService spyService = spy(weatherService);
+        Map<String, Object> weatherMap = new HashMap<>();
+        weatherMap.put("temperature", 15);
+        doReturn(weatherMap).when(spyService).fetchCurrent(59.3293, 18.0686);
+
+        Map <String, Object> response = spyService.fetchCurrentWeatherAtSpecificLocation(location);
+
+        assertNotNull(response);
+        assertEquals(15, response.get("temperature"));
+
     }
 }
 
