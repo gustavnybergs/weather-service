b31f9d28ac800142183dd76ab81155c2a967013e
StevenJTK <114819398+StevenJTK@users.noreply.github.com>
2025-09-18 12:34:12 +0200

    Refactor: HttpStatus for fetchLocation

--- src/main/java/com/grupp3/weather/service/WeatherService.java
+++ src/main/java/com/grupp3/weather/service/WeatherService.java
@@ -105,8 +105,7 @@ public class WeatherService {
 
         List<Map<String, Object>> results = (List<Map<String, Object>>) urlResponse.get("results");
         if (results == null || results.isEmpty()) {
-            System.out.println("Location not found." + location);
-            return null; // add exception later
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Location not found." + location);
         }
 
         return results.get(0);
